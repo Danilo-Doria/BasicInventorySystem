4b0fb2c42edbef0e4346c06de4dff02cbd3b28e0
Danilo Andres Doria Diaz <danilodoria519@gmail.com>
2026-03-11 22:04:43 -0500

    feat: create interactive menu

--- src/inventario.py
+++ src/inventario.py
@@ -1,43 +1,93 @@
-# Solicitar dato al usuario
-name = input("Ingrese el nombre del producto ")
-
-# Este while repite la pregunta al usuario siempre que ingrese un valor incorrecto
-while True:
-
-    # Este try lo que hace es atrapar lo que el usuario ingresa y si el tipo de variable no es la correcta
-    # entonces pasa al except mostrando el mensaje de error y el ciclo se repite, ya que nunca hubo break
-
-    try:
-        # Solicitar dato al usuario
-        price = float(input("Ingrese el precio del producto "))
-        # Solicitar dato al usuario
-        if price < 0:
-            print("Por favor ingrese valores numericos positivos")
-            continue
-        break
-    except ValueError:
-        print("Por favor ingrese solo valores numericos")
+inventory = []
+print("\nBienvenido al sistema de inventarios\n")
 
 while True:
-
     try:
-        # Solicitar dato al usuario
-        quantity = int(input("Ingrese el la cantidad del prodcuto "))
-        if quantity < 0:
-            print("Por favor ingrese valores numericos positivos")
+        print("1. Agregar producto")
+        print("2. Mostrar inventario")
+        print("3. Calcular estadísticas")
+        print("4. Salir")
+
+        option = int(input("\nIngresa una opcion: "))
+
+        if option not in (1, 2, 3, 4):
+            print("\nOpción inválida\n")
             continue
-        break
+
     except ValueError:
-        print("Por favor ingrese solo valores numericos")
+        print("\nPor favor solo digite valores numericos\n")
+        continue
 
-# Calculo del costo total, se obtiene multiplicando precio por cantidad
-costo_total = price * quantity
+    if option == 1:
 
-# Se imprime el nombre, precio, cantidad y el total del producto y se muestra por consola
-print(
-    f"Producto: {name} | Precio: {price} | Cantidad: {quantity} | Total: {costo_total}"
-)
+        # Solicitar dato al usuario
+        name = input("Ingrese el nombre del producto: ")
 
-# Este programa lo que hace basicamente es pedir por consola al usuario el nombre del producto, precio, y la cantidad
-# usando While True y try except validamos que lo que ingrese el usuario sea correcto, evitando asi errores
-# luego se calcula el costo total y se imprime por consola lo que el usuario ingresó junto con el total.
+        # Este while repite la pregunta al usuario siempre que ingrese un valor incorrecto
+        while True:
+
+            # Este try lo que hace es atrapar lo que el usuario ingresa y si el tipo de variable no es la correcta
+            # entonces pasa al except mostrando el mensaje de error y el ciclo se repite, ya que nunca hubo break
+
+            try:
+                # Solicitar dato al usuario
+                price = float(input("Ingrese el precio del producto: "))
+                # Solicitar dato al usuario
+                if price < 0:
+                    print("\nPor favor ingrese valores numericos positivos\n")
+                    continue
+                break
+            except ValueError:
+                print("\nPor favor ingrese solo valores numericos\n")
+
+        while True:
+
+            try:
+                # Solicitar dato al usuario
+                quantity = int(input("Ingrese el la cantidad del producto: "))
+                if quantity < 0:
+                    print("\nPor favor ingrese valores numericos positivos\n")
+                    continue
+                break
+            except ValueError:
+                print("\nPor favor ingrese solo valores numericos\n")
+
+        # Calculo del costo total, se obtiene multiplicando precio por cantidad
+        total_cost = price * quantity
+
+        # Se imprime el nombre, precio, cantidad y el total del producto y se muestra por consola
+        print(
+            f"\nProducto: {name} | Precio: {price} | Cantidad: {quantity} | Total: {total_cost}\n"
+        )
+
+        product = {
+            "nombre": name,
+            "precio": price,
+            "cantidad": quantity,
+        }
+
+        inventory.append(product)
+
+    elif option == 2:
+
+        if inventory == []:
+            print("\nInventario vacio!!\n")
+
+        else:
+
+            for product in inventory:
+                print(
+                    f"\nProducto: {product['nombre']} | Precio: {product['precio']} | Cantidad: {product['cantidad']}\n"
+                )
+
+    elif option == 3:
+        total_inventory_value = 0
+
+        for product in inventory:
+            total_inventory_value += product["precio"] * product["cantidad"]
+
+        print(f"\nEl valor total del inventario es: {total_inventory_value}\n")
+
+    else:
+        print("\nAdios")
+        break
